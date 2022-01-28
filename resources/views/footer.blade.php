63de6d6d54a14129459b5a906d642366333dedee
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2022-01-28 10:31:39 +0100

    Add Credentials

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -6,6 +6,14 @@
     <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
   </div>
   <!-- Copyright -->
+  <!-- Authors -->
+  <div class="footer-copyright text-center py-3">
+    <a href="https://github.com/Matimateokol"> Mateusz Kołacz</a>
+    <a href="https://github.com/Rados123"> Radosław Kulka</a>
+    <a href="https://github.com/kwyszynska"> Katarzyna Wyszyńska</a>
+    <a href="https://github.com/tomquess"> Tomasz Czarzbon</a>
+  </div>
+  <!-- Authors -->
 
 </footer>
 <!-- Footer -->
